dcd0de25f95caf6c226d4c36c9c80feee841988a
SamRol <1628526495@qq.com>
2023-11-30 01:41:01 +0800

    1.添加web.config文件 2.添加cors

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const enter = require("./router/enter");
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 const port = 3000;
-
+//process.env.PORT
 enter(app);
+
 
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
 })
 
 app.get("/", (req, res) => {
-   res.send("personal-express")
+   res.send("Welcome to SamRol-express")
 })
